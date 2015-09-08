0108e1ce56b5935881f8540191d523212ec8a7aa
Hasmikus <hasmikus@gmail.com>
2015-09-08 23:15:27 +0400

    Prototype of Point class

---
+++ point.h
@@ -0,0 +1,13 @@
+#ifndef Point_h
+#define Point_h
+
+class Point {
+      private:
+             int x,y;
+      public: 
+              Point (int x1, int y1);     
+              int getx ();
+              int gety ();
+};
+
+#endif
